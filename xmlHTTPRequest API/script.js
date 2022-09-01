60883c8d741ad07644fd4947ac043b5dc745a020
rpc <rpc7863@gamil.com>
2022-08-31 22:13:26 -0700

    xhr

--- xmlHTTPRequest API/script.js
+++ xmlHTTPRequest API/script.js
@@ -1,2 +1,12 @@
-const xhr = new XMLHttpRequest()
-console.log(xhr);
+var url = "https://jsonplaceholder.typicode.com/posts";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
